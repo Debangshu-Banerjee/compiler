87f694ba2bab817b6f5309bf031f9be1556c141a
Arpit Gupta <alphaWizard@users.noreply.github.com>
2019-02-16 21:29:10 +0530

    added assignment 2

---
+++ CS347-Assignment-2/c_analyzer.py
@@ -0,0 +1,132 @@
+import sys,os
+import re
+import xml.etree.ElementTree as ET
+from pyparsing import *
+
+xmlfile = "temp.xml"
+logfile = "logs.txt"
+
+def line_number(filename,index):
+    ct = 0
+    index += 1
+    line_no =1
+    with open(filename) as f:
+        for line in f.readlines():
+            ct += len(line)
+            if ct > index:
+                return line_no
+            line_no += 1
+
+def statements_count(filename):
+    lines = 0
+    with open(filename) as f:
+        for line in f.readlines():
+            lines += 1
+    return lines
+
+def comments_count(filename):
+    multiline_cmnt = Combine(Regex(r"/\*(?:[^*]|\*(?!/))*") + '*/').setName("c_comment")
+    singleline_cmnt = Regex(r"//(?:\\\n|[^\n])*").setName("// comment") # Comment of the form //
+    cppstyle_cmnt = Combine(multiline_cmnt | singleline_cmnt).setName("C++ style comment") #both sigle+multi
+    with open(filename) as f:
+        res = cppstyle_cmnt.scanString(f.read())
+        # cmnt_list = [tokens.start for tokens, startPos, EndPos in res]
+        cmnt_list = [(startPos,EndPos) for tokens, startPos, EndPos in res]
+        # print(cmnt_list)
+    return len(cmnt_list)
+    # return 0
+def total_commented_lines(filename):
+    multiline_cmnt = Combine(Regex(r"/\*(?:[^*]|\*(?!/))*") + '*/').setName("c_comment")
+    singleline_cmnt = Regex(r"//(?:\\\n|[^\n])*").setName("// comment") # Comment of the form //
+    cppstyle_cmnt = Combine(multiline_cmnt | singleline_cmnt).setName("C++ style comment") #both sigle+multi
+    with open(filename) as f:
+        res = cppstyle_cmnt.scanString(f.read())
+        cmnt_list = [(startPos,EndPos) for tokens, startPos, EndPos in res]
+        # print(len(cmnt_list))
+        total_lines = 0
+        for posn in cmnt_list:
+            # print(line_number(filename,posn[1]-1)-line_number(filename,posn[0])+1)
+            total_lines += line_number(filename,posn[1]-1)-line_number(filename,posn[0])+1
+        # print(cmnt_list)
+        return total_lines
+
+
+def blanklines_count(filename):
+    blanks = 0
+    with open(filename) as f:
+        for line in f.readlines():
+            if not line.strip():
+                blanks += 1
+    return blanks
+
+def macros_count(filename):
+    macrodef = "#" + ZeroOrMore(' ') + "define" + Word(alphas+"_",alphanums+"_").setResultsName("macro") + empty + restOfLine.setResultsName("value")
+    with open(filename) as f:
+        res = macrodef.scanString(f.read())
+        macros = [tokens.macro for tokens, startPos, EndPos in res]
+        # print(macros)
+    return len(macros)
+
+def variables_count(filename):
+    tree = ET.parse(xmlfile)
+    root = tree.getroot()
+    variables = []
+    for variable in root.findall('Variable'):
+        variables.append(variable.get('name'))
+    # print(variables)
+    return len(variables)
+
+
+def fdecl_count(filename):
+    tree = ET.parse(xmlfile)
+    root = tree.getroot()
+    function_lines = []
+    for function in root.findall('Function'):
+        function_lines.append(function.get('line'))
+    # print(function_lines)
+    fdecl = 0
+    with open(filename) as f:
+        filelines = f.readlines()
+        for line_no in function_lines:
+            if (filelines[int(line_no) - 1].strip())[-1:] == ';':
+                fdecl += 1
+                # print(filelines[int(line_no) - 1]) #func declaration lines
+
+    return fdecl
+
+def fdef_count(filename):
+    # with open(filename) as f:
+    #     for line in f.readlines():
+    #         do_something(line)
+    return total_functions(filename)-fdecl_count(filename)
+
+def total_functions(filename):
+    tree = ET.parse(xmlfile)
+    root = tree.getroot()
+    functions = []
+    for function in root.findall('Function'):
+        functions.append(function.get('name'))
+    # print(variables)
+    return len(functions)
+
+
+def analyzer(filename):
+    os.system("gccxml -std=c89 {} -fxml={} > {}".format(filename,xmlfile,logfile))
+    print("{}) source code statements   : {}".format(1,statements_count(filename)))
+    print("{}) comments                 : {}".format(2,total_commented_lines(filename)))
+    print("{}) blank lines              : {}".format(3,blanklines_count(filename)))
+    print("{}) macro definitions        : {}".format(4,macros_count(filename)))
+    print("{}) variable declarations    : {}".format(5,variables_count(filename)))
+    print("{}) function declarations    : {}".format(6,fdecl_count(filename)))
+    print("{}) function definitions     : {}".format(7,fdef_count(filename)))
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        filename  = sys.argv[1]
+    else:
+        filename = 'temp.c'   # default file
+    analyzer(filename)
+    # for i in range(15):
+    # print(line_number(filename,76))
