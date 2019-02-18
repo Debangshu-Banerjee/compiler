d63f695081bee84cd64ea3b8ed6ae9de2447167f
Arpit Gupta <alphaWizard@users.noreply.github.com>
2019-02-18 22:29:41 +0530

    added new test

---
+++ CS347-Assignment-2/test.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#define EOI             0       /* End of input                 */
+#define SEMI            1       /* ;                            */
+#define PLUS            2       /* +                            */
+#define TIMES           3       /* *                            */
+#define LP              4       /* (                            */
+#define RP              5       /* )                            */
+#  define NUM_OR_ID       6       /* Decimal Number or Identifier */
+
+extern char *yytext;            /* in lex.c                     */
+extern int yyleng;
+extern yylineno;
+int *y,*g;
+char *ar,*br;
+
+typedef int ff;
+
+/* multiline_cmnt  jjjjjjjjjjjjjjjjjjjjjjjj
+jvnjknvj m
+ggggggggggggggggggggggggggggggggggggggggg   8*/
+
+ // okay
+int h;
+
+struct ab{
+  int gk;
+};
+struct ab x,ty;
+
+ff d;
+
+int lgh(int,int);
+int lff(int a,int b) {return 50;}
+
+int main()
+{
+        /* statement_block */
+
+        return 0;
+
+}
