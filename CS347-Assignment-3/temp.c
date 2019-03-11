12b635948145d909ff07d252a1b99a38fad9e70b
Arpit Gupta <alphaWizard@users.noreply.github.com>
2019-03-11 22:57:23 +0530

    done partial 1st part of assignment 3

---
+++ CS347-Assignment-3/temp.c
@@ -0,0 +1,42 @@
+#define EOI             0       /* End of input                 */
+#define SEMI            1       /* ;                            */
+#define PLUS            2       /* +                            */
+#define TIMES           3       /* *                            */
+#define LP              4       /* (                            */
+#define RP              5       /* )                            */
+#define NUM_OR_ID       6       /* Decimal Number or Identifier */
+#define LOL 9
+#define LLLLL    1
+extern char *yytext;            /* in lex.c                     */
+extern int yyleng;
+// extern yylineno;
+
+//single line
+
+/*
+zdjgdjzv
+                 jsdfhjskdhvgkjsdkj
+                 jshkj  */
+int a,b,c;
+int *d=&b;
+
+int f(int,int);
+
+int bc(int a,int b)
+{
+return a+b;
+}
+
+/*ghgjjb  /* jshjsdb
+   jhgsjgh
+              dghdfhfg
+sdvgahsvfh dbschjs */
+
+int main()
+{
+        /* statement_block */
+        return 0;
+}
+
+int f(int a,int b)
+{ return a-b; }
