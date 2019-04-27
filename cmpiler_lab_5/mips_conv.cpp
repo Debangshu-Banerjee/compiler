5470044553865d71be451fade001c32ee41af220
Debangshu Banerjee <31929616+Debangshu-Banerjee@users.noreply.github.com>
2019-04-27 23:34:26 +0530

    added initial mips

--- cmpiler_lab_5/mips_conv.cpp
+++ cmpiler_lab_5/mips_conv.cpp
@@ -9,6 +9,46 @@ vector<string> data_items;
 vector<string> data_size;
 ofstream mipsfile;
 
+void genarate_micro_op_int(string op,string opr1,string opr2,string result){
+  int first=0;mipsfile<<"la $t"<<first<<", "<<opr1<<endl;
+  int secnd=1;mipsfile<<"la $t"<<secnd<<", "<<opr2<<endl;
+  int thrd=2;mipsfile<<"la $t"<<thrd<<", "<<result<<endl;
+  int frth=3;mipsfile<<"lw $t"<<frth<<", "<<"0($t"<<first<<")"<<endl;
+  int fifth=4;mipsfile<<"lw $t"<<fifth<<", "<<"0($t"<<secnd<<")"<<endl;
+  int sixth=5;mipsfile<<op<<" $t"<<sixth<<", "<<"$t"<<frth<<", "<<"$t"<<fifth<<endl;
+  mipsfile<<"sw $t"<<sixth<<", 0($t"<<thrd<<")"<<endl;
+}
+void genarate_micro_op_int_div(string op,string opr1,string opr2,string result){
+  int first=0;mipsfile<<"la $t"<<first<<", "<<opr1<<endl;
+  int secnd=1;mipsfile<<"la $t"<<secnd<<", "<<opr2<<endl;
+  int thrd=2;mipsfile<<"la $t"<<thrd<<", "<<result<<endl;
+  int frth=3;mipsfile<<"lw $t"<<frth<<", "<<"0($t"<<first<<")"<<endl;
+  int fifth=4;mipsfile<<"lw $t"<<fifth<<", "<<"0($t"<<secnd<<")"<<endl;
+  int sixth=5;mipsfile<<op<<" $t"<<frth<<", "<<"$t"<<fifth<<endl;
+  mipsfile<<"mflo "<<"$t5 "<<endl;
+  mipsfile<<"sw $t"<<sixth<<", 0($t"<<thrd<<")"<<endl;
+}
+void genarate_micro_op_float(string op,string opr1,string opr2,string result){
+  int first=0;mipsfile<<"la $t"<<first<<", "<<opr1<<endl;
+  int secnd=1;mipsfile<<"la $t"<<secnd<<", "<<opr2<<endl;
+  int thrd=2;mipsfile<<"la $t"<<thrd<<", "<<result<<endl;
+  int frth=3;mipsfile<<"l.s $f"<<frth<<", "<<"0($t"<<first<<")"<<endl;
+  int fifth=4;mipsfile<<"l.s $f"<<fifth<<", "<<"0($t"<<secnd<<")"<<endl;
+  int sixth=5;mipsfile<<op<<" $f"<<sixth<<", "<<"$f"<<frth<<", "<<"$f"<<fifth<<endl;
+  mipsfile<<"s.s $f"<<sixth<<", 0($t"<<thrd<<")"<<endl;
+
+}
+void genarate_micro_op_float_int(string op,string opr1,string opr2,string result){
+  int first=0;mipsfile<<"la $t"<<first<<", "<<opr1<<endl;
+  int secnd=1;mipsfile<<"la $t"<<secnd<<", "<<opr2<<endl;
+  int thrd=2;mipsfile<<"la $t"<<thrd<<", "<<result<<endl;
+  int frth=3;mipsfile<<"l.s $f"<<frth<<", "<<"0($t"<<first<<")"<<endl;
+  int fifth=4;mipsfile<<"l.s $f"<<fifth<<", "<<"0($t"<<secnd<<")"<<endl;
+  mipsfile<<"cvt.s.w $f"<<frth<<", "<<"$f"<<frth<<endl;
+  mipsfile<<"cvt.s.w $f"<<fifth<<", "<<"$f"<<fifth<<endl;
+  int sixth=5;mipsfile<<op<<" $f"<<sixth<<", "<<"$f"<<frth<<", "<<"$f"<<fifth<<endl;
+  mipsfile<<"s.s $f"<<sixth<<", 0($t"<<thrd<<")"<<endl;
+}
 vector<std::string> split(string line){
   // string tok;
   // stringstream ss(line);
@@ -39,7 +79,7 @@ void readfile(string filename){
 }
 
 void generate_data_segment(){
-    mipsfile <<".data"<< endl;
+    mipsfile<<".data"<< endl;
     for(int i=0;i< data_items.size();i++){
         mipsfile<< data_items[i] <<":"<< " .space " <<data_size[i]<<endl; // needs attention fora array declaration
     }
@@ -58,158 +98,87 @@ int tokenise_data_segment(){
       data_size.push_back(tokens[2]);
     }
   }
+  return lines.size();
 }
 
-int gen_int_count()
-{
-  static int ct=-1;
-  ct++;
-  return ct;
-}
-
-int gen_float_count()
-{
-  static int ct=-1;
-  ct++;
-  return ct;
-}
 
 void generate_each_instruction(vector<string> linevec)
 {
   //mipsfile << text << endl;
-  if(linevec.size()<=1) return;
+  if(linevec.size()<=4) return;
+  if(linevec[1].empty() || linevec[2].empty() || linevec[3].empty() || linevec[4].empty()){
+      return;
+  }
   if(linevec[1]=="+")
   {
     if(linevec[2][0]=='T'&&linevec[3][0]=='T'&&linevec[4][0]=='T')
     {
-      int first=0;mipsfile<<"la $t"<<first<<" "<<linevec[2]<<endl;
-      int secnd=1;mipsfile<<"la $t"<<secnd<<" "<<linevec[3]<<endl;
-      int thrd=2;mipsfile<<"la $t"<<thrd<<" "<<linevec[4]<<endl;
-      int frth=3;mipsfile<<"lw $t"<<frth<<" "<<"0($t"<<first<<")"<<endl;
-      int fifth=4;mipsfile<<"lw $t"<<fifth<<" "<<"0($t"<<secnd<<")"<<endl;
-      int sixth=5;mipsfile<<"add "<<"$t"<<sixth<<" "<<"$t"<<frth<<" "<<"$t"<<fifth<<endl;
-      mipsfile<<"sw $t"<<sixth<<" 0($t"<<thrd<<")"<<endl;
+      genarate_micro_op_int("add",linevec[2],linevec[3],linevec[4]);
     }
     else if(linevec[2][0]=='F'&&linevec[3][0]=='F'&&linevec[4][0]=='F')
     {
-      int first=0;mipsfile<<"la $t"<<first<<" "<<linevec[2]<<endl;
-      int secnd=1;mipsfile<<"la $t"<<secnd<<" "<<linevec[3]<<endl;
-      int thrd=2;mipsfile<<"la $t"<<thrd<<" "<<linevec[4]<<endl;
-      int frth=3;mipsfile<<"l.s $f"<<frth<<" "<<"0($t"<<first<<")"<<endl;
-      int fifth=4;mipsfile<<"l.s $f"<<fifth<<" "<<"0($t"<<secnd<<")"<<endl;
-      int sixth=5;mipsfile<<"add.s "<<"$f"<<sixth<<" "<<"$f"<<frth<<" "<<"$f"<<fifth<<endl;
-      mipsfile<<"s.s $f"<<sixth<<" 0($t"<<thrd<<")"<<endl;
+      genarate_micro_op_float("add.s",linevec[2],linevec[3],linevec[4]);
     }
-
+    else{
+      genarate_micro_op_float_int("add.s",linevec[2],linevec[3],linevec[4]);
+    }
   }
   if(linevec[1]=="-")
   {
     if(linevec[2][0]=='T'&&linevec[3][0]=='T'&&linevec[4][0]=='T')
     {
-      int first=0;mipsfile<<"la $t"<<first<<" "<<linevec[2]<<endl;
-      int secnd=1;mipsfile<<"la $t"<<secnd<<" "<<linevec[3]<<endl;
-      int thrd=2;mipsfile<<"la $t"<<thrd<<" "<<linevec[4]<<endl;
-      int frth=3;mipsfile<<"lw $t"<<frth<<" "<<"0($t"<<first<<")"<<endl;
-      int fifth=4;mipsfile<<"lw $t"<<fifth<<" "<<"0($t"<<secnd<<")"<<endl;
-      int sixth=5;mipsfile<<"sub "<<"$t"<<sixth<<" "<<"$t"<<frth<<" "<<"$t"<<fifth<<endl;
-      mipsfile<<"sw $t"<<sixth<<" 0($t"<<thrd<<")"<<endl;
+      genarate_micro_op_int("sub",linevec[2],linevec[3],linevec[4]);
     }
     else if(linevec[2][0]=='F'&&linevec[3][0]=='F'&&linevec[4][0]=='F')
     {
-      int first=0;mipsfile<<"la $t"<<first<<" "<<linevec[2]<<endl;
-      int secnd=1;mipsfile<<"la $t"<<secnd<<" "<<linevec[3]<<endl;
-      int thrd=2;mipsfile<<"la $t"<<thrd<<" "<<linevec[4]<<endl;
-      int frth=3;mipsfile<<"l.s $f"<<frth<<" "<<"0($t"<<first<<")"<<endl;
-      int fifth=4;mipsfile<<"l.s $f"<<fifth<<" "<<"0($t"<<secnd<<")"<<endl;
-      int sixth=5;mipsfile<<"sub.s "<<"$f"<<sixth<<" "<<"$f"<<frth<<" "<<"$f"<<fifth<<endl;
-      mipsfile<<"s.s $f"<<sixth<<" 0($t"<<thrd<<")"<<endl;
+      genarate_micro_op_float("sub.s",linevec[2],linevec[3],linevec[4]);
+    }
+    else{
+      genarate_micro_op_float_int("sub.s",linevec[2],linevec[3],linevec[4]);
     }
   }
   if(linevec[1]=="*")
   {
     if(linevec[2][0]=='T'&&linevec[3][0]=='T'&&linevec[4][0]=='T')
     {
-      int first=0;mipsfile<<"la $t"<<first<<" "<<linevec[2]<<endl;
-      int secnd=1;mipsfile<<"la $t"<<secnd<<" "<<linevec[3]<<endl;
-      int thrd=2;mipsfile<<"la $t"<<thrd<<" "<<linevec[4]<<endl;
-      int frth=3;mipsfile<<"lw $t"<<frth<<" "<<"0($t"<<first<<")"<<endl;
-      int fifth=4;mipsfile<<"lw $t"<<fifth<<" "<<"0($t"<<secnd<<")"<<endl;
-      int sixth=5;mipsfile<<"mul "<<"$t"<<sixth<<" "<<"$t"<<frth<<" "<<"$t"<<fifth<<endl;
-      mipsfile<<"sw $t"<<sixth<<" 0($t"<<thrd<<")"<<endl;
+      genarate_micro_op_int("mul",linevec[2],linevec[3],linevec[4]);
     }
     else if(linevec[2][0]=='F'&&linevec[3][0]=='F'&&linevec[4][0]=='F')
     {
-      int first=0;mipsfile<<"la $t"<<first<<" "<<linevec[2]<<endl;
-      int secnd=1;mipsfile<<"la $t"<<secnd<<" "<<linevec[3]<<endl;
-      int thrd=2;mipsfile<<"la $t"<<thrd<<" "<<linevec[4]<<endl;
-      int frth=3;mipsfile<<"l.s $f"<<frth<<" "<<"0($t"<<first<<")"<<endl;
-      int fifth=4;mipsfile<<"l.s $f"<<fifth<<" "<<"0($t"<<secnd<<")"<<endl;
-      int sixth=5;mipsfile<<"mul.s "<<"$f"<<sixth<<" "<<"$f"<<frth<<" "<<"$f"<<fifth<<endl;
-      mipsfile<<"s.s $f"<<sixth<<" 0($t"<<thrd<<")"<<endl;
+      genarate_micro_op_float("mul.s",linevec[2],linevec[3],linevec[4]);
+    }
+    else{
+      genarate_micro_op_float_int("mul.s",linevec[2],linevec[3],linevec[4]);
     }
   }
   if(linevec[1]=="/")
   {
     if(linevec[2][0]=='T'&&linevec[3][0]=='T'&&linevec[4][0]=='T')
     {
-      int first=0;mipsfile<<"la $t"<<first<<" "<<linevec[2]<<endl;
-      int secnd=1;mipsfile<<"la $t"<<secnd<<" "<<linevec[3]<<endl;
-      int thrd=2;mipsfile<<"la $t"<<thrd<<" "<<linevec[4]<<endl;
-      int frth=3;mipsfile<<"lw $t"<<frth<<" "<<"0($t"<<first<<")"<<endl;
-      int fifth=4;mipsfile<<"lw $t"<<fifth<<" "<<"0($t"<<secnd<<")"<<endl;
-      int sixth=5;mipsfile<<"div "<<"$t"<<sixth<<" "<<"$t"<<frth<<" "<<"$t"<<fifth<<endl;
-      mipsfile<<"sw $t"<<sixth<<" 0($t"<<thrd<<")"<<endl;
+        genarate_micro_op_int_div("div",linevec[2],linevec[3],linevec[4]);
     }
     else if(linevec[2][0]=='F'&&linevec[3][0]=='F'&&linevec[4][0]=='F')
     {
-      int first=0;mipsfile<<"la $t"<<first<<" "<<linevec[2]<<endl;
-      int secnd=1;mipsfile<<"la $t"<<secnd<<" "<<linevec[3]<<endl;
-      int thrd=2;mipsfile<<"la $t"<<thrd<<" "<<linevec[4]<<endl;
-      int frth=3;mipsfile<<"l.s $f"<<frth<<" "<<"0($t"<<first<<")"<<endl;
-      int fifth=4;mipsfile<<"l.s $f"<<fifth<<" "<<"0($t"<<secnd<<")"<<endl;
-      int sixth=5;mipsfile<<"div.s "<<"$f"<<sixth<<" "<<"$f"<<frth<<" "<<"$f"<<fifth<<endl;
-      mipsfile<<"s.s $f"<<sixth<<" 0($t"<<thrd<<")"<<endl;
+      genarate_micro_op_float("div.s",linevec[2],linevec[3],linevec[4]);
+    }
+    else{
+      genarate_micro_op_float_int("div.s",linevec[2],linevec[3],linevec[4]);
     }
   }
-  if(linevec[1]=="=")
-  {
-
-  }
-  if(linevec[1]=="==")
-  {
-
-  }
-  if(linevec[1]==">")
-  {
-
-  }
-  if(linevec[1]==">=")
-  {
-
-  }
-  if(linevec[1]=="goto")
-  {
-    //cout<<"goto";
-  }
-
 }
 
 void generate()
 {
-  int index=tokenise_data_segment();
+  int index = tokenise_data_segment();
   generate_data_segment();
   fstream file;
   int line;
+  mipsfile<<".text"<<endl;
   for(line = index; line < lines.size(); line++){
     vector<string> linevec = split(lines[line]);  //split
-    //cout<<linevec.size()<<endl;
     generate_each_instruction(linevec);
   }
-  // vector<string> ff=split(lines[0]);
-  // for(int i=0;i<ff.size();++i)
-  // {
-  //   cout<<ff[i]<<endl;
-  // }
+
 }
 
 int main(int argc, char** argv)
